fc461f8e616384ab5c4b8d84ce713fb05e915c4d
mostazomarc <105717367+mostazomarc@users.noreply.github.com>
2024-03-07 12:45:03 +0100

    tasca #51 Botó de play i canvi d'estats. Botons colocats i estilitzats i barra de progrés provisional

--- App/greeny/lib/City/city.dart
+++ App/greeny/lib/City/city.dart
@@ -18,10 +18,33 @@ class _CityPageState extends State<CityPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text('City'),
-            IconButton(onPressed: play, icon: const Icon(Icons.play_arrow)),
-            IconButton(onPressed: viewHistory, icon: const Icon(Icons.restore))
+            Container(
+              width: 70,
+              height: 70,
+              margin:
+                  EdgeInsets.all(8.0), // Ajusta el margen según tus necesidades
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(22.0),
+                color: const Color.fromARGB(255, 1, 167,
+                    164), // Cambia el color del fondo redondeado aquí
+              ),
+
+              child: IconButton(
+                onPressed: play,
+                icon: const Icon(Icons.play_arrow, color: Colors.white),
+                iconSize: 40.0,
+              ),
+            ),
+            BarraProgres(),
           ],
         ),
+      ),
+      appBar: AppBar(
+        title: Text('Greeny'),
+        leading: IconButton(
+            onPressed: viewHistory,
+            icon: const Icon(Icons.restore),
+            color: const Color.fromARGB(255, 1, 167, 164)),
       ),
     );
   }
@@ -32,5 +55,30 @@ class _CityPageState extends State<CityPage> {
 
   void viewHistory() {
     print('Viewing history');
+  }
+}
+
+class BarraProgres extends StatefulWidget {
+  @override
+  _BarraProgresState createState() => _BarraProgresState();
+}
+
+class _BarraProgresState extends State<BarraProgres> {
+  double progres = 0.5;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        LinearProgressIndicator(
+          value: progres,
+          backgroundColor: Colors.grey,
+          valueColor: AlwaysStoppedAnimation<Color>(
+              Colors.blue), // Ajusta el color de la barra de progreso aquí
+        ),
+        SizedBox(height: 10.0),
+        Text('Progreso: ${(progres * 100).toStringAsFixed(1)}%'),
+      ],
+    );
   }
 }
