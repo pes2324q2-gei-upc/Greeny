7daccab0eb92678e8f630ffe1e91bb10d6b0c20d
miquel.amorin <miquel.amorin@estudiantat.upc.edu>
2024-03-07 10:01:50 +0100

    map works

--- App/greeny/lib/Map/map.dart
+++ App/greeny/lib/Map/map.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -12,58 +11,22 @@ class MapPage extends StatefulWidget {
 class _MapPageState extends State<MapPage> {
   late GoogleMapController mapController;
 
-  LatLng _center = const LatLng(45.521563, -122.677433);
-
-  LocationData? currentLocation;
-
-  void getLocation() async {
-    Location location = Location();
-
-    location.enableBackgroundMode(enable: true);
-
-    var _serviceEnabled = await location.serviceEnabled();
-    if (!_serviceEnabled) {
-      _serviceEnabled = await location.requestService();
-      if (!_serviceEnabled) {
-        return;
-      }
-    }
-
-    var _permissionGranted = await location.hasPermission();
-    if (_permissionGranted == PermissionStatus.denied) {
-      _permissionGranted = await location.requestPermission();
-      if (_permissionGranted != PermissionStatus.granted) {
-        return;
-      }
-    }
-
-    currentLocation = await location.getLocation();
-    _center = LatLng(currentLocation!.latitude!, currentLocation!.longitude!);
-  }
+  final LatLng _center = const LatLng(45.521563, -122.677433);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
 
   @override
-  void initState() {
-    getLocation();
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-          markers: {
-            Marker(
-                markerId: const MarkerId('currentLocation'), position: _center)
-          }),
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 11.0,
+        ),
+      ),
     );
   }
 }
