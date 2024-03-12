9d41f99871fe9c0fdd93fc7c726e74cb85ce1b5c
miquel.amorin <miquel.amorin@estudiantat.upc.edu>
2024-03-12 22:06:25 +0100

    resolve conflicts

--- App/greeny/lib/Profile/settings.dart
+++ App/greeny/lib/Profile/settings.dart
@@ -10,7 +10,6 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  String _selectedLanguage = 'English';
 
   @override
   Widget build(BuildContext context) {
