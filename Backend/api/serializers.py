52ca765b937da1dcd5592cb005af398a6b0f1ef8
Ivan <ivbuirez@gmail.com>
2024-03-13 09:41:06 +0100

    Tasca #69 finalitzada

--- Backend/api/serializers.py
+++ Backend/api/serializers.py
@@ -29,39 +29,3 @@ class StopSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stop
         exclude = ['id']
-
-# class TramSerializer(serializers.ModelSerializer):
-#     class Meta(EstacioSerializer.Meta):
-#         model = Tram
-#         fields = EstacioSerializer.Meta.fields + ['linies']
-
-# class MetroSerializer(serializers.ModelSerializer):
-#     class Meta(EstacioSerializer.Meta):
-#         model = Metro
-#         fields = EstacioSerializer.Meta.fields + ['linies']
-
-# class FGCSerializer(serializers.ModelSerializer):
-#     class Meta(EstacioSerializer.Meta):
-#         model = FGC
-#         fields = EstacioSerializer.Meta.fields + ['linies']
-
-
-# class RENFESerializer(serializers.ModelSerializer):
-#     class Meta(EstacioSerializer.Meta):
-#         model = RENFE
-#         fields = EstacioSerializer.Meta.fields + ['linies']
-
-# class BicingSerializer(serializers.ModelSerializer):
-#     class Meta(EstacioSerializer.Meta):
-#         model = Bicing
-#         fields = EstacioSerializer.Meta.fields + ['capacitat']
-
-# class BUSSerializer(serializers.ModelSerializer):
-#     class Meta(EstacioSerializer.Meta):
-#         model = BUS
-#         fields = EstacioSerializer.Meta.fields + ['linies']
-
-# class PuntsRecarregaSerializer(serializers.ModelSerializer):
-#     class Meta(EstacioSerializer.Meta):
-#         model = PuntsRecarrega
-#         fields = EstacioSerializer.Meta.fields + ['acces', 'velocitatCarrega', 'potencia', 'tipusCorrent', 'tipusConnexio']
