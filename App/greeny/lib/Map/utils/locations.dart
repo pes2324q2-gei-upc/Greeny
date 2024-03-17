2eb922c147e2b156edc786b75e5a68823d7a2a53
miquel.amorin <miquel.amorin@estudiantat.upc.edu>
2024-03-17 11:13:28 +0100

    lazy loading markers

--- App/greeny/lib/Map/utils/locations.dart
+++ App/greeny/lib/Map/utils/locations.dart
@@ -10,20 +10,36 @@ part 'locations.g.dart';
 
 @JsonSerializable()
 class Locations {
-  Locations({
-    required this.stations,
-  });
+  Stations stations;
+
+  Locations({required this.stations});
 
   factory Locations.fromJson(Map<String, dynamic> json) =>
       _$LocationsFromJson(json);
   Map<String, dynamic> toJson() => _$LocationsToJson(this);
-
-  final List<Station> stations;
 }
 
 @JsonSerializable()
-class Station {
-  Station({
+class Stations {
+  List<PublicTransportStation> publicTransportStations;
+  List<BusStation> busStations;
+  List<BicingStation> bicingStations;
+  List<ChargingStation> chargingStations;
+
+  Stations(
+      {required this.publicTransportStations,
+      required this.busStations,
+      required this.bicingStations,
+      required this.chargingStations});
+
+  factory Stations.fromJson(Map<String, dynamic> json) =>
+      _$StationsFromJson(json);
+  Map<String, dynamic> toJson() => _$StationsToJson(this);
+}
+
+@JsonSerializable()
+class PublicTransportStation {
+  PublicTransportStation({
     required this.stops,
     required this.name,
     required this.rating,
@@ -31,15 +47,86 @@ class Station {
     required this.longitude,
   });
 
-  factory Station.fromJson(Map<String, dynamic> json) =>
-      _$StationFromJson(json);
-  Map<String, dynamic> toJson() => _$StationToJson(this);
+  factory PublicTransportStation.fromJson(Map<String, dynamic> json) =>
+      _$PublicTransportStationFromJson(json);
+  Map<String, dynamic> toJson() => _$PublicTransportStationToJson(this);
 
   final List<Stop> stops;
   final double latitude;
   final double longitude;
   final String name;
   final double rating;
+}
+
+@JsonSerializable()
+class BusStation {
+  BusStation({
+    required this.lines,
+    required this.name,
+    required this.rating,
+    required this.latitude,
+    required this.longitude,
+  });
+
+  factory BusStation.fromJson(Map<String, dynamic> json) =>
+      _$BusStationFromJson(json);
+  Map<String, dynamic> toJson() => _$BusStationToJson(this);
+
+  final List<String> lines;
+  final double latitude;
+  final double longitude;
+  final String name;
+  final double rating;
+}
+
+@JsonSerializable()
+class BicingStation {
+  BicingStation({
+    required this.capacitat,
+    required this.name,
+    required this.rating,
+    required this.latitude,
+    required this.longitude,
+  });
+
+  factory BicingStation.fromJson(Map<String, dynamic> json) =>
+      _$BicingStationFromJson(json);
+  Map<String, dynamic> toJson() => _$BicingStationToJson(this);
+
+  final int capacitat;
+  final double latitude;
+  final double longitude;
+  final String name;
+  final double rating;
+}
+
+@JsonSerializable()
+class ChargingStation {
+  ChargingStation({
+    required this.name,
+    required this.rating,
+    required this.latitude,
+    required this.longitude,
+    required this.acces,
+    required this.charging_velocity,
+    required this.power,
+    required this.current_type,
+    required this.connexion_type,
+  });
+
+  factory ChargingStation.fromJson(Map<String, dynamic> json) =>
+      _$ChargingStationFromJson(json);
+  Map<String, dynamic> toJson() => _$ChargingStationToJson(this);
+
+  final double latitude;
+  final double longitude;
+  final String name;
+  final double rating;
+  final String acces;
+  final String charging_velocity;
+  final int power;
+  final String current_type;
+  final String connexion_type;
 }
 
 @JsonSerializable()
