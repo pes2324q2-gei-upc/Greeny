2c0ac396567799db5eb96af1ecc301a123b6b2eb
Miquel Amorín <64908581+miqueelamorin@users.noreply.github.com>
2024-03-28 09:11:29 +0100

    Update settings.py

--- Backend/greeny/settings.py
+++ Backend/greeny/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
         'PORT': 5432,
+        'HOST': 'localhost',
     }
 }
 
