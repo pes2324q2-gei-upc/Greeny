fb241d3dfbe7e2dbf26379eecc4ceca852d60df8
Miquel Amorín <miquel.amorin@estudiantat.upc.edu>
2024-04-21 13:17:43 +0200

    tasca 91 finalitzada

--- App/greeny/lib/appState.dart
+++ App/greeny/lib/appState.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:greeny/Map/utils/locations.dart';
 import 'package:provider/provider.dart';
 
 class AppState with ChangeNotifier {
@@ -7,10 +9,72 @@ class AppState with ChangeNotifier {
   double _totalDistance = 0.0;
   late Position? _previousPosition =
       null; // posicio anterior per calcular els km desplaçats
+  Set<Marker> markers = {};
+  Map<String, bool> transports = {
+    'tram': false,
+    'bus': false,
+    'fgc': false,
+    'bicing': false,
+    'renfe': false,
+    'car': false,
+    'metro': false
+  };
+  CameraPosition cameraPosition =
+      const CameraPosition(target: LatLng(0, 0), zoom: 16);
 
   bool get isPlaying => _isPlaying;
   double get totalDistance => _totalDistance;
   Position? get previousPosition => _previousPosition;
+
+  Locations stations = Locations(
+      stations: Stations(
+          publicTransportStations: [],
+          busStations: [],
+          bicingStations: [],
+          chargingStations: []));
+
+  Map icons = {
+    'BUS': null,
+    'BICING': null,
+    'CAR': null,
+    'FGC': null,
+    'METRO': null,
+    'RENFE': null,
+    'TRAM': null
+  };
+
+  void setIcons(Map newIcons) {
+    icons = newIcons;
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      notifyListeners();
+    });
+  }
+
+  void setStations(Locations newStations) {
+    stations = newStations;
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      notifyListeners();
+    });
+  }
+
+  void setCameraPosition(CameraPosition newCameraPosition) {
+    cameraPosition = newCameraPosition;
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      notifyListeners();
+    });
+  }
+
+  void setMarkers(Set<Marker> newMarkers) {
+    markers = newMarkers;
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      notifyListeners();
+    });
+  }
+
+  void setTransports(Map<String, bool> newTransports) {
+    transports = newTransports;
+    notifyListeners();
+  }
 
   set isPlaying(bool value) {
     _isPlaying = value;
