cb8ba026b7d63c5217b07215178a77a86b9e2582
marc.mostazo <marc.mostazo@estudiantat.upc.edu>
2024-04-23 15:08:27 +0200

    Tasca #85. Primera versió del perfil personal.

--- App/greeny/lib/Profile/profile.dart
+++ App/greeny/lib/Profile/profile.dart
@@ -6,6 +6,9 @@ import 'package:greeny/Profile/settings.dart';
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
+  static const Color smallCardColor = Color.fromARGB(255, 240, 235, 235);
+  static const Color titolColor = Color.fromARGB(255, 133, 131, 131);
+
   @override
   State<ProfilePage> createState() => _ProfilePageState();
 }
@@ -56,17 +59,217 @@ class _ProfilePageState extends State<ProfilePage> {
           ],
         )),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                userName,
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+          child: Container(
+            margin: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Stack(
+                  children: [
+                    const SizedBox(
+                      height: 20, // Espacio entre la imagen y la tarjeta
+                    ),
+                    SizedBox(
+                      width: 120,
+                      height: 120,
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(100),
+                          child: Image.asset(
+                            'assets/images/perfil.jpeg',
+                            fit: BoxFit.cover,
+                          )),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        width: 35,
+                        height: 35,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            color: const Color.fromARGB(255, 1, 167, 164)),
+                        child: const Icon(
+                          Icons.edit,
+                          color: Colors.black,
+                          size: 20,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
+                const SizedBox(
+                  height: 10, // Espacio entre la imagen y la tarjeta
+                ),
+                Text(
+                  userName,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20, // Espacio entre la imagen y la tarjeta
+                ),
+                Expanded(
+                  child: Container(
+                    width: double.infinity, // Ocupa todo el ancho disponible
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                        bottomLeft: Radius.circular(30),
+                        bottomRight: Radius.circular(30),
+                      ),
+                      color: Colors.white, // Color de fondo de la tarjeta
+                    ),
+                    padding:
+                        const EdgeInsets.all(20), // Espaciado interno opcional
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        //aqui va la informació de l'usuari
+                        const SizedBox(height: 10),
+                        Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: ProfilePage.smallCardColor),
+                          padding: const EdgeInsets.all(10),
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.email,
+                                  color: ProfilePage
+                                      .titolColor), // Icono de correo electrónico
+                              SizedBox(
+                                  width:
+                                      5), // Espacio entre el icono y el correo electrónico
+                              Text(
+                                'Email:', // Correo electrónico
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: ProfilePage.titolColor,
+                                ),
+                              ),
+                              Spacer(),
+                              Text(
+                                'correo@ejemplo.com', // Correo electrónico
+                                style: TextStyle(
+                                  fontSize: 16,
+                                ),
+                              ),
+                              SizedBox(width: 5),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: ProfilePage.smallCardColor),
+                          padding: const EdgeInsets.all(10),
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.calendar_month,
+                                  color: ProfilePage
+                                      .titolColor), // Icono de correo electrónico
+                              SizedBox(
+                                  width:
+                                      5), // Espacio entre el icono y el correo electrónico
+                              Text(
+                                'Joined:', // Correo electrónico
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: ProfilePage.titolColor,
+                                ),
+                              ),
+                              Spacer(),
+                              Text(
+                                '24/03/24', // Correo electrónico
+                                style: TextStyle(
+                                  fontSize: 16,
+                                ),
+                              ),
+                              SizedBox(width: 5),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: ProfilePage.smallCardColor),
+                          padding: const EdgeInsets.all(10),
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.star,
+                                  color: ProfilePage
+                                      .titolColor), // Icono de correo electrónico
+                              SizedBox(
+                                  width:
+                                      5), // Espacio entre el icono y el correo electrónico
+                              Text(
+                                'Level:', // Correo electrónico
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: ProfilePage.titolColor,
+                                ),
+                              ),
+                              Spacer(),
+                              Text(
+                                '100', // Correo electrónico
+                                style: TextStyle(
+                                  fontSize: 16,
+                                ),
+                              ),
+                              SizedBox(width: 5),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: ProfilePage.smallCardColor),
+                          padding: const EdgeInsets.all(10),
+                          child: const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.people,
+                                color: ProfilePage.titolColor,
+                              ), // Icono de correo electrónico
+                              SizedBox(
+                                  width:
+                                      5), // Espacio entre el icono y el correo electrónico
+                              Text(
+                                'Friends:', // Correo electrónico
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: ProfilePage.titolColor,
+                                ),
+                              ),
+                              Spacer(),
+                              Text(
+                                '200', // Correo electrónico
+                                style: TextStyle(
+                                  fontSize: 16,
+                                ),
+                              ),
+                              SizedBox(width: 5),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20, // Espacio entre la imagen y la tarjeta
+                ),
+              ],
+            ),
           ),
         ),
       );
