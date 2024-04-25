68badb142343bd156dda1c94da2474ac82cbc6c8
albalr <albalopez656@gmail.com>
2024-04-25 19:35:01 +0200

    calculs CO2 fets

--- Backend/api/utils.py
+++ Backend/api/utils.py
@@ -1,33 +1,43 @@
 def calculate_co2_consumed(transports, total_distance):
     # Calculate the CO2 consumed by the user
     # 0.0 kg CO2 per km for walking and biking
-    # 0.12 kg CO2 per km for public transport
-    # 0.22 kg CO2 per km for bus
-    # 0.29 kg CO2 per km for motorcycle
-    # 0.17 kg CO2 per km for car
-    # 0.15 kg CO2 per km for electric car
+    # 0.08074 kg CO2 per km for Bus
+    # 0.053 kg CO2 per km for Motorcycle
+    # 0.143 kg CO2 per km for Car (Gasoline)
+    # 0.070 kg CO2 per km for Electric Car
+    # 0.05013 kg CO2 per km for Metro
+    # 0.08012 kg CO2 per km for Tram
+    # 0.03577 kg CO2 per km for FGC
+    # 0.04688 kg CO2 per km for Train
 
+    transport_dist = total_distance / len(transports)
     co2_consumed = 0.0
     for transport in transports:
         if transport == 'Walking' or transport == 'Bike':
             co2_consumed += 0.0
-        elif transport == 'Train, Metro, Tram, FGC':
-            co2_consumed += 0.12 * total_distance
+        elif transport == 'Metro':
+            co2_consumed += 0.05013 * transport_dist
+        elif transport == 'Tram':
+            co2_consumed += 0.08012 * transport_dist
+        elif transport == 'FGC':
+            co2_consumed += 0.03577 * transport_dist
+        elif transport == 'Train':
+            co2_consumed += 0.04688 * transport_dist
         elif transport == 'Bus':
-            co2_consumed += 0.22 * total_distance
+            co2_consumed += 0.08074 * transport_dist
         elif transport == 'Motorcycle':
-            co2_consumed += 0.29 * total_distance
+            co2_consumed += 0.053 * transport_dist
         elif transport == 'Car':
-            co2_consumed += 0.17 * total_distance
+            co2_consumed += 0.143 * transport_dist
         elif transport == 'Electric Car':
-            co2_consumed += 0.15 * total_distance
+            co2_consumed += 0.070 * transport_dist
 
     return co2_consumed
 
 def calculate_car_co2_consumed(total_distance):
     # Calculate the CO2 consumed by the user if they had used a car
-    # 0.17 kg CO2 per km for car
-    return 0.17 * total_distance
+    # 0.143 kg CO2 per km for Car (Gasoline)
+    return 0.143 * total_distance
 
 def calculate_statistics(transports, total_distance):
     update_fields = {
@@ -72,4 +82,3 @@ def calculate_statistics(transports, total_distance):
         update_fields['km_Totals'] = total_distance
 
     return update_fields
-
