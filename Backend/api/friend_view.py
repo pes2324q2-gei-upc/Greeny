c0e3f543e3b0854e4cd4c6435aad6e496463deba
miquel.amorin <miquel.amorin@upc.edu>
2024-04-25 12:37:13 +0200

    friends hotfix

--- Backend/api/friend_view.py
+++ Backend/api/friend_view.py
@@ -1,18 +1,15 @@
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.exceptions import AuthenticationFailed
 from .models import Friend_Request, User
 from .serializers import FriendRequestSerializer, FriendSerializer, FriendUserSerializer
 
 class FriendRequestViewSet(viewsets.ViewSet):
-    authentication_classes = [TokenAuthentication]
+    authentication_classes = [JWTAuthentication]
 
     def create(self, request):
 
-        user, token = self.request.user, self.request.auth
-
-        from_user = user
+        from_user = self.request.user
         to_user = User.objects.get(id=request.data['to_user'])
         friend_request, created = Friend_Request.objects.get_or_create(from_user=from_user, to_user=to_user)
 
@@ -22,20 +19,14 @@ class FriendRequestViewSet(viewsets.ViewSet):
             return Response({'message': 'friend request already sent'}, status=status.HTTP_409_CONFLICT)
 
     def list(self, request):
-        try:
-            user, token = self.request.user, self.request.auth
-        except AuthenticationFailed:
-            return Response({'error': 'Invalid token'}, status=status.HTTP_401_UNAUTHORIZED)
+        user = self.request.user
 
         friend_requests = Friend_Request.objects.filter(to_user=user)
         serializer = FriendRequestSerializer(friend_requests, many=True)
         return Response(serializer.data)
 
     def destroy(self, request, pk=None):
-        try:
-            user, token = self.request.user, self.request.auth
-        except AuthenticationFailed:
-            return Response({'error': 'Invalid token'}, status=status.HTTP_401_UNAUTHORIZED)
+        user = self.request.user
 
         friend_request = Friend_Request.objects.get(id=pk)
         if friend_request.to_user == user:
@@ -47,11 +38,11 @@ class FriendRequestViewSet(viewsets.ViewSet):
             return Response({'message': 'friend request not accepted'}, status=status.HTTP_409_CONFLICT)
 
 class FriendViewSet(viewsets.ViewSet):
-    authentication_classes = [TokenAuthentication]
+    authentication_classes = [JWTAuthentication]
 
     def list(self, request):
 
-        user, token = self.request.user, self.request.auth
+        user= self.request.user
         friends = user.friends.all()
         serializer = FriendUserSerializer(friends, many=True)
         return Response(serializer.data)
