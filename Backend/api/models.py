3177ef592ba2c81029a39bfe5d5ed70afa14a07e
Miquel Amorín <miquel.amorin@estudiantat.upc.edu>
2024-04-30 08:30:32 +0200

    refactor

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -118,7 +118,7 @@ class Route(models.Model):
         ('Car', 'Car')
     ]
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='routes')
     distance = models.FloatField(default=0.0)
     transports = ArrayField(
         models.CharField(max_length=30, choices=TRANSPORT_MODES)
@@ -157,7 +157,7 @@ class Level(models.Model):
     current = models.BooleanField(default=False)
     points_user = models.IntegerField(default=0)
     points_total = models.IntegerField(default=0)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)  
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='level')  
     neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE) 
     
 
