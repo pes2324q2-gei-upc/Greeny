1ab7283b3cd90f4ad718b2e7ae9e84ca193b415f
marc.mostazo <marc.mostazo@estudiantat.upc.edu>
2024-04-30 16:05:40 +0200

    Tasca #98. Pantalla Amics. Primera versió pantalla amics

---
+++ App/greeny/lib/Friends/friend_profile.dart
@@ -0,0 +1,400 @@
+import 'dart:convert';
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_translate/flutter_translate.dart';
+import 'package:greeny/API/requests.dart';
+import 'package:greeny/API/user_auth.dart';
+import 'package:greeny/Profile/edit_profile.dart';
+import 'package:greeny/Profile/settings.dart';
+import 'package:intl/intl.dart';
+
+class FriendProfilePage extends StatefulWidget {
+  final String friendUsername;
+
+  const FriendProfilePage({Key? key, required this.friendUsername})
+      : super(key: key);
+
+  static const Color smallCardColor = Color.fromARGB(255, 240, 235, 235);
+  static const Color titolColor = Color.fromARGB(255, 133, 131, 131);
+
+  @override
+  State<FriendProfilePage> createState() => _FriendProfilePageState();
+}
+
+class _FriendProfilePageState extends State<FriendProfilePage> {
+  String friendName = '';
+  String dateJoined = '';
+  int level = 0;
+  int friends = 0;
+  int trips = 0;
+  int reviews = 0;
+
+  @override
+  void initState() {
+    getInfoUser();
+    super.initState();
+  }
+
+  Future<void> getInfoUser() async {
+    List<dynamic> userData = [];
+    final String endpoint = '/api/user/${widget.friendUsername}';
+
+    final response = await httpGet('endpoint');
+
+    if (response.statusCode == 200) {
+      setState(() {
+        userData = json.decode(response.body);
+        friendName = userData[0]['first_name'];
+        dateJoined = DateFormat('dd-MM-yyyy')
+            .format(DateTime.parse(userData[0]['date_joined']));
+        level = userData[0]['level'];
+        friends = userData[0]['friends_number'];
+        trips = userData[0]['routes_number'];
+        reviews = userData[0]['reviews_number'];
+      });
+    } else {
+      showMessage("Error loading user info");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (friendName == '') {
+      return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 220, 255, 255),
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 220, 255, 255),
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back),
+            color: const Color.fromARGB(255, 1, 167, 164),
+          ),
+        ),
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    } else {
+      return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 220, 255, 255),
+        appBar: (AppBar(
+          backgroundColor: const Color.fromARGB(255, 220, 255, 255),
+          title: Text(translate('Profile'),
+              style: const TextStyle(fontWeight: FontWeight.bold)),
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back),
+            color: const Color.fromARGB(255, 1, 167, 164),
+          ),
+        )),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              margin: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Stack(
+                    children: [
+                      const SizedBox(
+                        height: 20, // Espacio entre la imagen y la tarjeta
+                      ),
+                      Container(
+                        width: 120,
+                        height: 120,
+                        decoration: BoxDecoration(
+                          shape: BoxShape
+                              .circle, // Establece la forma como un círculo
+                          border: Border.all(
+                            color: const Color.fromARGB(
+                                255, 1, 167, 164), // Color del borde
+                            width: 5, // Ancho del borde
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey
+                                  .withOpacity(0.5), // Color de la sombra
+                              spreadRadius: 3, // Extensión de la sombra
+                              blurRadius: 3, // Desenfoque de la sombra
+                              offset: const Offset(
+                                  0, 2), // Desplazamiento de la sombra
+                            ),
+                          ],
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(
+                              60), // Radio de borde igual a la mitad del ancho/alto
+                          child: Image.asset(
+                            'assets/images/perfil.jpeg',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    '@${widget.friendUsername}',
+                    style: const TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: FriendProfilePage.titolColor,
+                    ),
+                  ),
+                  Text(
+                    friendName,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                        bottomLeft: Radius.circular(30),
+                        bottomRight: Radius.circular(30),
+                      ),
+                      color: Colors.white, // Color de fondo de la tarjeta
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey
+                              .withOpacity(0.2), // Color de la sombra
+                          spreadRadius: 5, // Extensión de la sombra
+                          blurRadius: 7, // Desenfoque de la sombra
+                          offset:
+                              const Offset(0, 3), // Desplazamiento de la sombra
+                        ),
+                      ],
+                    ),
+                    constraints: BoxConstraints(
+                      minWidth: double.infinity,
+                      minHeight: 0,
+                    ),
+                    child: SingleChildScrollView(
+                      //padding: const EdgeInsets.only(bottom: 20),
+                      child: Container(
+                        width:
+                            double.infinity, // Ocupa todo el ancho disponible
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            topRight: Radius.circular(30),
+                            bottomLeft: Radius.circular(30),
+                            bottomRight: Radius.circular(30),
+                          ),
+                          color: Colors.white, // Color de fondo de la tarjeta
+                        ),
+                        padding: const EdgeInsets.all(20), // Espaciado interno
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            //aqui va la informació de l'usuari
+                            const SizedBox(height: 5),
+                            //Data registre
+                            Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  color: FriendProfilePage.smallCardColor),
+                              padding: const EdgeInsets.all(10),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Icon(Icons.calendar_month,
+                                      color: FriendProfilePage.titolColor),
+                                  const SizedBox(width: 5),
+                                  Text(
+                                    translate('Joined:'),
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      color: FriendProfilePage.titolColor,
+                                    ),
+                                  ),
+                                  const Spacer(),
+                                  Text(
+                                    dateJoined,
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 5),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            //Nivell
+                            Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  color: FriendProfilePage.smallCardColor),
+                              padding: const EdgeInsets.all(10),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Icon(Icons.star,
+                                      color: FriendProfilePage.titolColor),
+                                  const SizedBox(width: 5),
+                                  Text(
+                                    translate('Level:'),
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      color: FriendProfilePage.titolColor,
+                                    ),
+                                  ),
+                                  const Spacer(),
+                                  Text(
+                                    level.toString(),
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 5),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            //Amics
+                            Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  color: FriendProfilePage.smallCardColor),
+                              padding: const EdgeInsets.all(10),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Icon(
+                                    Icons.people,
+                                    color: FriendProfilePage.titolColor,
+                                  ),
+                                  const SizedBox(width: 5),
+                                  Text(
+                                    translate('Friends:'),
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      color: FriendProfilePage.titolColor,
+                                    ),
+                                  ),
+                                  const Spacer(),
+                                  Text(
+                                    friends.toString(),
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 5),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            //Viatges
+                            Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  color: FriendProfilePage.smallCardColor),
+                              padding: const EdgeInsets.all(10),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Icon(
+                                    Icons.route,
+                                    color: FriendProfilePage.titolColor,
+                                  ),
+                                  const SizedBox(width: 5),
+                                  Text(
+                                    translate('Trips:'),
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      color: FriendProfilePage.titolColor,
+                                    ),
+                                  ),
+                                  const Spacer(),
+                                  Text(
+                                    trips.toString(),
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 5),
+                                ],
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            //Reviews
+                            Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  color: FriendProfilePage.smallCardColor),
+                              padding: const EdgeInsets.all(10),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Icon(
+                                    Icons.reviews,
+                                    color: FriendProfilePage.titolColor,
+                                  ),
+                                  const SizedBox(width: 5),
+                                  Text(
+                                    translate('Reviews:'),
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold,
+                                      color: FriendProfilePage.titolColor,
+                                    ),
+                                  ),
+                                  const Spacer(),
+                                  Text(
+                                    reviews.toString(),
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 5),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+  }
+
+  void showMessage(String m) {
+    if (mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(translate(m)),
+          duration: const Duration(seconds: 10),
+          behavior: SnackBarBehavior.floating,
+          backgroundColor: Theme.of(context).colorScheme.primary,
+        ),
+      );
+    }
+  }
+
+  void share() {}
+}
--- App/greeny/lib/Friends/friends.dart
+++ App/greeny/lib/Friends/friends.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_translate/flutter_translate.dart';
+import 'package:greeny/Friends/friend_profile.dart';
 
 class FriendsPage extends StatefulWidget {
   const FriendsPage({super.key});
@@ -9,14 +10,107 @@ class FriendsPage extends StatefulWidget {
 }
 
 class _FriendsPageState extends State<FriendsPage> {
+  List<String> friends = ['Marc', 'Arnau', 'Friend 3', 'Friend 4'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 220, 255, 255),
       appBar: AppBar(
-        title: Text(translate('Friends'), style: const TextStyle(fontWeight: FontWeight.bold)),
+        title: Text(translate('Friends'),
+            style: const TextStyle(fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center),
         backgroundColor: const Color.fromARGB(255, 220, 255, 255),
+        actions: [
+          IconButton(
+            onPressed: addFriend,
+            icon: const Icon(Icons.person_add),
+            color: const Color.fromARGB(255, 1, 167, 164),
+          ),
+        ],
+      ),
+      body: GridView.builder(
+        padding: EdgeInsets.all(10),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, // Dos tarjetas por fila
+          crossAxisSpacing: 10, // Espacio horizontal entre las tarjetas
+          mainAxisSpacing: 10, // Espacio vertical entre las tarjetas
+        ),
+        itemCount: friends.length,
+        itemBuilder: (BuildContext context, int index) {
+          return buildFriendCard(
+              friends[index]); // Construye la tarjeta del amigo
+        },
       ),
     );
+  }
+
+  Widget buildFriendCard(String friendUsername) {
+    return GestureDetector(
+      onTap: () {
+        // Navegar al perfil del amigo
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
+                FriendProfilePage(friendUsername: friendUsername),
+          ),
+        );
+      },
+      child: Card(
+        elevation: 3, // Elevación de la tarjeta
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15), // Bordes redondeados
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle, // Establece la forma como un círculo
+                  border: Border.all(
+                    color: const Color.fromARGB(
+                        255, 1, 167, 164), // Color del borde
+                    width: 5, // Ancho del borde
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5), // Color de la sombra
+                      spreadRadius: 3, // Extensión de la sombra
+                      blurRadius: 3, // Desenfoque de la sombra
+                      offset: const Offset(0, 2), // Desplazamiento de la sombra
+                    ),
+                  ],
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(
+                      60), // Radio de borde igual a la mitad del ancho/alto
+                  child: Image.asset(
+                    'assets/images/perfil.jpeg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                friendUsername, // Nombre del amigo
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void addFriend() {
+    // Add friend code here
   }
 }
