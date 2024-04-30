5544897f871bf538a2ced5c7e431680175a9e1b5
mostazomarc <105717367+mostazomarc@users.noreply.github.com>
2024-04-30 09:42:55 +0200

    Revert "loading screen"
    This reverts commit a3874dedfd75891310002dace9b5b9be11778249.

--- App/greeny/lib/main.dart
+++ App/greeny/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:greeny/app_state.dart';
-import 'package:greeny/loading_screen.dart';
 import 'package:greeny/main_page.dart';
 import 'package:provider/provider.dart';
 import 'Registration/log_in.dart';
@@ -79,7 +78,7 @@ class Greeny extends StatelessWidget {
           future: mainScreenIfUser(),
           builder: (BuildContext context, AsyncSnapshot<Widget> snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
-              return const LoadingPage();
+              return const CircularProgressIndicator();
             } else {
               if (snapshot.hasError) {
                 return Text('Error: ${snapshot.error}');
@@ -96,7 +95,7 @@ class Greeny extends StatelessWidget {
 
 Future<Widget> mainScreenIfUser() async {
   String token = await getToken();
-
+  
   if (token != '') {
     return const MainPage();
   } else {
