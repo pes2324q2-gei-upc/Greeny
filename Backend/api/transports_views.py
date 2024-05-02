990d87bacf1cc86808e6522358499f436a6607ef
Ivan <ivbuirez@gmail.com>
2024-05-02 19:07:26 +0200

    refactor transport views, fixed tests and pylint

--- Backend/api/transports_views.py
+++ Backend/api/transports_views.py
@@ -117,104 +117,109 @@ class FetchPublicTransportStations(View):
 
         for item in stations:
             full_name = item.get("EQUIPAMENT")
-            #Metro: ej METRO (L1) - CATALUNYA
             if "METRO" in full_name:
-                station_name = full_name.split(" - ")[1].replace("-","")
-                line = [full_name.split("(")[1].split(")")[0]]
-
-
-                #Miramos si existe el tipo Metro, sino creamos la instancia
-                trans_type = self.get_type(TransportType.TTransport.METRO)
-
-                #Buscamos por nombre por si ya existe la parada
-                station = self.get_public_transport_station(station_name)
-
-                if station is None:                     #Si no existe estacionTPublic la creamos
-
-                    new_station_tp = self.create_public_transport_station(item, station_name)
-
-                    #Creamos la parada asociada a la estacion
-                    Stop.objects.create(
-                        station = new_station_tp,
-                        transport_type = trans_type,
-                        lines = line
-                    )
-
-                else: #Si existe la parada, actualizamos las lineas del Metro Asociado
-                    try:
-                        stop = Stop.objects.get(station=station, transport_type=trans_type)
-                        stop.lines = stop.lines + line
-                        stop.save()
-                    except Stop.DoesNotExist:
-                        Stop.objects.create(station=station, transport_type=trans_type, lines=line)
-
-            #Tramvia: ej TRAMVIA (T1,T2) - LES AIGÜES-
-            if "TRAM" in full_name:
-                station_name = full_name.split(" - ")[1].replace("-","")
-                stop_lines = (full_name.split(" - ")[0].replace(" ", "")
-                              .split("(")[1].replace(")", "").split(","))
-
-                if station_name == "Mª CRISTINA":
-                    station_name = "MARIA CRISTINA"
-                elif station_name == "TORREBLANCA":
-                    stop_lines = [full_name.split(" - ")[0].split(" ")[2].replace(")", "")]
-
-                #Buscamos si existe la estacion; sino la creamos
-                station = self.get_public_transport_station(station_name)
-
-                trans_type = self.get_type(TransportType.TTransport.TRAM)
-
-                if station is None:
-                    station = self.create_public_transport_station(item, station_name)
-
-                #creamos parada asociada
-                Stop.objects.create(station=station, transport_type=trans_type, lines=stop_lines)
-
-            # RENFE: ej RENFE - CATALUNYA-
+                self.process_metro(item, full_name)
+            elif "TRAM" in full_name:
+                self.process_tram(item, full_name)
             elif "RENFE" in full_name:
-                if "(RENFE)" in full_name:
-                    station_name = full_name.split(" (")[0]
-                else:
-                    try:
-                        station_name = full_name.split(" - ")[1].replace("-","")
-                    except IndexError:
-                        continue
-
-                station = self.get_public_transport_station(station_name)
-                trans_type = self.get_type(TransportType.TTransport.RENFE)
-
-                if station is None:
-                    station = self.create_public_transport_station(item, station_name)
-
-                try:
-                    stop = Stop.objects.get(station=station, transport_type=trans_type)
-                except Stop.DoesNotExist:
-                    Stop.objects.create(station=station, transport_type=trans_type, lines=[])
-
-            #FGC ej: FGC - CATALUNYA (C. de Rossello)-
-            #       FGC - L'HOSPITALET-AV.CARRILET-
-            #       FGC - ESPANYA-
+                self.process_renfe(item, full_name)
             elif "FGC" in full_name:
+                self.process_renfe(item, full_name)
 
-                if "/ FGC" in full_name:
-                    station_name = "DIAGONAL"
-                elif "- FGC-" in full_name:
-                    station_name = full_name.split(" - ")[0]
-                else:
-                    station_name = full_name.split(" - ")[1][:-1]
-                    if '(' in station_name:
-                        station_name = station_name.split(" (")[0]
+    def process_metro(self, item, full_name):
+        station_name = full_name.split(" - ")[1].replace("-","")
+        line = [full_name.split("(")[1].split(")")[0]]
 
-                station = self.get_public_transport_station(station_name)
-                trans_type = self.get_type(TransportType.TTransport.FGC)
 
-                if station is None:
-                    station = self.create_public_transport_station(item, station_name)
+        #Miramos si existe el tipo Metro, sino creamos la instancia
+        trans_type = self.get_type(TransportType.TTransport.METRO)
 
-                try:
-                    stop = Stop.objects.get(station=station, transport_type=trans_type)
-                except Stop.DoesNotExist:
-                    Stop.objects.create(station=station, transport_type=trans_type, lines=[])
+        #Buscamos por nombre por si ya existe la parada
+        station = self.get_public_transport_station(station_name)
+
+        if station is None:                     #Si no existe estacionTPublic la creamos
+
+            new_station_tp = self.create_public_transport_station(item, station_name)
+
+            #Creamos la parada asociada a la estacion
+            Stop.objects.create(
+                station = new_station_tp,
+                transport_type = trans_type,
+                lines = line
+            )
+
+        else: #Si existe la parada, actualizamos las lineas del Metro Asociado
+            try:
+                stop = Stop.objects.get(station=station, transport_type=trans_type)
+                stop.lines = stop.lines + line
+                stop.save()
+            except Stop.DoesNotExist:
+                Stop.objects.create(station=station, transport_type=trans_type, lines=line)
+
+    def process_tram(self, item, full_name):
+        station_name = full_name.split(" - ")[1].replace("-","")
+        stop_lines = (full_name.split(" - ")[0].replace(" ", "")
+                        .split("(")[1].replace(")", "").split(","))
+
+        if station_name == "Mª CRISTINA":
+            station_name = "MARIA CRISTINA"
+        elif station_name == "TORREBLANCA":
+            stop_lines = [full_name.split(" - ")[0].split(" ")[2].replace(")", "")]
+
+        #Buscamos si existe la estacion; sino la creamos
+        station = self.get_public_transport_station(station_name)
+
+        trans_type = self.get_type(TransportType.TTransport.TRAM)
+
+        if station is None:
+            station = self.create_public_transport_station(item, station_name)
+
+        #creamos parada asociada
+        Stop.objects.create(station=station, transport_type=trans_type, lines=stop_lines)
+
+
+    def process_renfe(self, item, full_name):
+        if "(RENFE)" in full_name:
+            station_name = full_name.split(" (")[0]
+        else:
+            try:
+                station_name = full_name.split(" - ")[1].replace("-","")
+            except IndexError:
+                return None
+
+        station = self.get_public_transport_station(station_name)
+        trans_type = self.get_type(TransportType.TTransport.RENFE)
+
+        if station is None:
+            station = self.create_public_transport_station(item, station_name)
+
+        try:
+            Stop.objects.get(station=station, transport_type=trans_type)
+        except Stop.DoesNotExist:
+            Stop.objects.create(station=station, transport_type=trans_type, lines=[])
+
+        return None
+
+    def process_fgc(self, item, full_name):
+        if "/ FGC" in full_name:
+            station_name = "DIAGONAL"
+        elif "- FGC-" in full_name:
+            station_name = full_name.split(" - ")[0]
+        else:
+            station_name = full_name.split(" - ")[1][:-1]
+            if '(' in station_name:
+                station_name = station_name.split(" (")[0]
+
+        station = self.get_public_transport_station(station_name)
+        trans_type = self.get_type(TransportType.TTransport.FGC)
+
+        if station is None:
+            station = self.create_public_transport_station(item, station_name)
+
+        try:
+            Stop.objects.get(station=station, transport_type=trans_type)
+        except Stop.DoesNotExist:
+            Stop.objects.create(station=station, transport_type=trans_type, lines=[])
 
     def get(self, request):
 
@@ -223,8 +228,7 @@ class FetchPublicTransportStations(View):
         self.fetch_bicing()
         self.fetch_charging_stations()
 
-        data = {"status" : "fetched_successfully"}
-        return Response(data)
+        return Response({"status" : "fetched_successfully"}, status.HTTP_200_OK)
 
 class StationsView(APIView):
     def get_station(self, pk, station_type):
