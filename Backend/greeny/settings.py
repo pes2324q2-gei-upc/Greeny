345e9a381c2ff5b98725e5122099465c3048c6a8
Miquel Amorín <miquel.amorin@estudiantat.upc.edu>
2024-05-02 09:44:09 +0200

    password al serializer

--- Backend/greeny/settings.py
+++ Backend/greeny/settings.py
@@ -59,7 +59,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'django_extensions',
-    'rest_framework.authtoken',
     'django.contrib.gis',
 ]
 
@@ -143,12 +142,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
