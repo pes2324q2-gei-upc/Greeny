8cd0ca714ef2e08371866f5c34964bfe0ea61e7e
miquel.amorin <miquel.amorin@upc.edu>
2024-05-09 10:43:35 +0200

    tasca 119 google login back finalitzat

--- Backend/api/google_views.py
+++ Backend/api/google_views.py
@@ -1,12 +1,45 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+import jwt
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import AllowAny
 from .models import User
 from .serializers import UserSerializer
 
-def GoogleAuth(APIView):
+class GoogleAuth(APIView):
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
 
     def post(self, request):
+        # Get the Google token from the request data
+        google_token = request.data.get('token')
+
+        # Decode the Google token
+        try:
+            payload = jwt.decode(google_token, options={"verify_signature": False})
+        except jwt.InvalidTokenError:
+            return Response({'error': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Get the user's data from the token payload
+        email = payload.get('email')
+        name = payload.get('given_name')
+        username = email.split('@')[0]
+
+        # Check if a user with this email exists
+        user = User.objects.filter(username=username).first()
+
+        if user is None:
+            # If the user doesn't exist, create a new user
+            user = User.objects.create(email=email, first_name=name, username=username)
+
+        # Generate a JWT token for the user
+        refresh = RefreshToken.for_user(user)
+        token = {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
+
+        return Response(token, status=status.HTTP_200_OK)
         
     
