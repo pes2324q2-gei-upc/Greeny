e054d58548df7db2fd217625421cbd44def750ce
marc.mostazo <marc.mostazo@estudiantat.upc.edu>
2024-05-23 16:05:19 +0200

    Tasca #133. Ranking. Es pot anar a friendsPage des del Ranking.

--- App/greeny/lib/Social/rank.dart
+++ App/greeny/lib/Social/rank.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_translate/flutter_translate.dart';
+import 'package:greeny/Friends/friends.dart';
 
 class RankPage extends StatefulWidget {
   const RankPage({super.key});
@@ -17,18 +18,29 @@ class _RankPageState extends State<RankPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 220, 255, 255),
+        appBar: AppBar(
+          title: Text(translate('Ranking'),
+              style: const TextStyle(fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center),
+          backgroundColor: const Color.fromARGB(255, 220, 255, 255),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.people_outline_rounded),
+              color: const Color.fromARGB(255, 1, 167, 164),
+              onPressed: () {
+                friends();
+              },
+            )
+          ],
+        ),
         body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SizedBox(height: 100),
-          Text(
-            translate('Ranking'),
-            style: const TextStyle(fontWeight: FontWeight.bold),
-          ),
-        ],
-      ),
-    ));
+          child: Column(),
+        ));
+  }
+
+  void friends() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => const FriendsPage()));
   }
 }
