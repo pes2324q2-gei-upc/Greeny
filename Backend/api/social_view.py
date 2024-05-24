c3007a680b91aada781b8e8bfd974e4e55bf9ab8
marc.mostazo <marc.mostazo@estudiantat.upc.edu>
2024-05-24 18:15:37 +0200

    Tasca #133. Ranking. Limit d'usuaris ranking i comentaris.

--- Backend/api/social_view.py
+++ Backend/api/social_view.py
@@ -4,17 +4,26 @@ from .serializers import FriendUserSerializer
 from .models import User
 
 class RankingViewSet(viewsets.ViewSet):
+    """
+    API endpoint que permet mostrar el ranking d'usuaris
+    """
+    DEFAULT_LIMIT = 20 #Numero d'usuaris a mostrar per defecte
 
     def list(self, request):
+        """
+        Retorna el ranking d'usuaris
+        """
         user = request.user
         filter_type = request.query_params.get('filter', 'all')
 
+        limit = self.DEFAULT_LIMIT
+
         if filter_type == 'friends':
             friends = user.friends.all()
-            friends = user.friends.all()  # Asumiendo que tienes una relación de amigos definida en el modelo User
-            queryset = User.objects.filter(id__in=friends).order_by('-points')
+            friends = user.friends.all()
+            queryset = User.objects.filter(id__in=friends).order_by('-points')[:limit]
         else:
-            queryset = User.objects.all().order_by('-points')
+            queryset = User.objects.all().order_by('-points')[:limit]
 
         serializer = FriendUserSerializer(queryset, many=True, context={'request': request})
         return Response(serializer.data, status=status.HTTP_200_OK)
