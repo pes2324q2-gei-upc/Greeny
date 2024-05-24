d61784b3f9246b9ff4c6fd39d88e48d33fd7a622
marc.mostazo <marc.mostazo@estudiantat.upc.edu>
2024-05-24 19:23:38 +0200

    Tasca #133. Ranking. Podium TOP 3 implementat i funcional.

---
+++ App/greeny/lib/Social/podium.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class PodiumAvatar extends StatelessWidget {
+  final String profileImage;
+  final int rank;
+  final String username;
+  final int points;
+
+  const PodiumAvatar(
+      {required this.profileImage,
+      required this.rank,
+      required this.username,
+      required this.points,
+      Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 150,
+      width: 120,
+      child: Stack(
+        children: [
+          // Imagen de perfil
+          Positioned(
+            top: 25,
+            child: Container(
+              width: 110,
+              height: 110,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 3,
+                    blurRadius: 3,
+                    offset: const Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: CircleAvatar(backgroundImage: NetworkImage(profileImage)),
+            ),
+          ),
+          // Número de posición
+          Positioned(
+            left: 42.5,
+            top: 120,
+            child: CircleAvatar(
+              backgroundColor: const Color.fromARGB(255, 1, 167, 164),
+              radius: 12,
+              child: Text(
+                rank.toString(),
+                style: TextStyle(fontSize: 10),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            top: 150,
+            width: 110,
+            child: Text(
+              username,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            top: 175,
+            width: 110,
+            child: Text(
+              points.toString(),
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 15, color: Colors.grey[600]),
+            ),
+          ),
+          if (rank == 1)
+            Positioned(
+              left: 0,
+              top: 0,
+              width: 110,
+              child: Center(
+                child: SvgPicture.asset(
+                  'assets/svg/crown.svg',
+                  height: 40,
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
--- App/greeny/lib/Social/rank.dart
+++ App/greeny/lib/Social/rank.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_translate/flutter_translate.dart';
+import 'package:greeny/API/requests.dart';
 import 'package:greeny/Friends/friends.dart';
+import 'package:greeny/Social/podium.dart';
 
 class RankPage extends StatefulWidget {
   const RankPage({super.key});
@@ -10,37 +14,144 @@ class RankPage extends StatefulWidget {
 }
 
 class _RankPageState extends State<RankPage> {
+  List<User> _users = [];
+
   @override
   void initState() {
     super.initState();
+    _fetchRanking();
+  }
+
+  Future<void> _fetchRanking() async {
+    try {
+      // Hacer la solicitud a la API
+      final response = await httpGet('/api/ranking/');
+      if (response.statusCode == 200) {
+        // Analizar la respuesta JSON
+        final List<dynamic> jsonData = json.decode(response.body);
+        final List<User> users = jsonData.map((e) => User.fromJson(e)).toList();
+        setState(() {
+          _users = users;
+        });
+      } else {
+        throw Exception('Failed to load ranking');
+      }
+    } catch (error) {
+      print('Error fetching ranking: $error');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 220, 255, 255),
+      appBar: AppBar(
+        title: Text(translate('Ranking'),
+            style: const TextStyle(fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center),
         backgroundColor: const Color.fromARGB(255, 220, 255, 255),
-        appBar: AppBar(
-          title: Text(translate('Ranking'),
-              style: const TextStyle(fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center),
-          backgroundColor: const Color.fromARGB(255, 220, 255, 255),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.people_outline_rounded),
-              color: const Color.fromARGB(255, 1, 167, 164),
-              onPressed: () {
-                friends();
-              },
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.people_outline_rounded),
+            color: const Color.fromARGB(255, 1, 167, 164),
+            onPressed: () {
+              friends();
+            },
+          )
+        ],
+      ),
+      body: _users.isEmpty
+          ? const Center(
+              child: CircularProgressIndicator(),
             )
-          ],
-        ),
-        body: Center(
-          child: Column(),
-        ));
+          : Center(
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal:
+                        20.0), // Ajusta el valor del margen según sea necesario
+                child: Container(
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        // Usuarios en el podio
+                        if (_users.length > 1)
+                          Transform.scale(
+                            scale: 0.65,
+                            child: PodiumAvatar(
+                              profileImage: _users[1].avatar,
+                              rank: 2,
+                              username: _users[1].username,
+                              points: _users[1].points,
+                            ),
+                          ),
+                        if (_users.isNotEmpty)
+                          PodiumAvatar(
+                            profileImage: _users[0].avatar,
+                            rank: 1,
+                            username: _users[0].username,
+                            points: _users[0].points,
+                          ),
+                        if (_users.length > 2)
+                          Transform.scale(
+                            scale: 0.65,
+                            child: PodiumAvatar(
+                              profileImage: _users[2].avatar,
+                              rank: 3,
+                              username: _users[2].username,
+                              points: _users[2].points,
+                            ),
+                          ),
+                        // Agregar más usuarios según sea necesario
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+      // body: _users.isEmpty
+      //     ? const Center(
+      //         child: CircularProgressIndicator(),
+      //       )
+      //     : ListView.builder(
+      //         itemCount: _users.length,
+      //         itemBuilder: (context, index) {
+      //           final user = _users[index];
+      //           return ListTile(
+      //             title: Text(user.username),
+      //             subtitle: Text('Points: ${user.points}'),
+      //             // Agregar CircleAvatar para mostrar la foto de perfil
+      //             leading: CircleAvatar(
+      //               backgroundImage: NetworkImage(user.avatar),
+      //               radius:
+      //                   25, // Ajusta el tamaño de la imagen según sea necesario
+      //             ),
+      //           );
+      //         },
+      //       ),
+    );
   }
 
   void friends() {
     Navigator.push(
         context, MaterialPageRoute(builder: (context) => const FriendsPage()));
+  }
+}
+
+class User {
+  final String avatar;
+  final String username;
+  final int points;
+
+  User({required this.avatar, required this.username, required this.points});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      avatar: json['image'],
+      username: json['username'],
+      points: json['points'],
+    );
   }
 }
