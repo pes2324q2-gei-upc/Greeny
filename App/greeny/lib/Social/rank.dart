732ed145c8bf5ea7ec580ff456716554ebbf78c1
marc.mostazo <marc.mostazo@estudiantat.upc.edu>
2024-05-25 11:04:11 +0200

    Tasca #133. Ranking. Es pot fer swipe down per refrescar el ranking.
    Es pot fer swipe down a la llista no al podium.

--- App/greeny/lib/Social/rank.dart
+++ App/greeny/lib/Social/rank.dart
@@ -134,184 +134,190 @@ class _RankPageState extends State<RankPage> {
           ? const Center(
               child: CircularProgressIndicator(),
             )
-          : Center(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal:
-                        20.0), // Ajusta el valor del margen según sea necesario
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () => _toggleFilter(false),
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: _filterFriends
-                                ? Colors.grey[300]
-                                : const Color.fromARGB(255, 1, 167, 164),
-                          ),
-                          child: const Text('All'),
-                        ),
-                        const SizedBox(width: 10),
-                        ElevatedButton(
-                          onPressed: () => _toggleFilter(true),
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: _filterFriends
-                                ? const Color.fromARGB(255, 1, 167, 164)
-                                : Colors.grey[300],
-                          ),
-                          child: const Text('Friends'),
-                        ),
-                      ],
-                    ),
-                    // Usuarios en el podio
-                    SizedBox(
-                      height: 250,
-                      width: double.infinity,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          : RefreshIndicator(
+              onRefresh: getFriendsRequests,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal:
+                          20.0), // Ajusta el valor del margen según sea necesario
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          if (_users != null) ...[
-                            if (_users.length > 1)
-                              Transform.scale(
-                                scale: 0.65,
-                                child: PodiumAvatar(
-                                  profileImage: _users[1].avatar,
-                                  rank: 2,
-                                  username: _users[1].username,
-                                  points: _users[1].points,
-                                  mastery: _users[1].mastery,
-                                  level: _users[1].level,
-                                ),
-                              ),
-                            if (_users.isNotEmpty)
-                              PodiumAvatar(
-                                profileImage: _users[0].avatar,
-                                rank: 1,
-                                username: _users[0].username,
-                                points: _users[0].points,
-                                mastery: _users[0].mastery,
-                                level: _users[0].level,
-                              ),
-                            if (_users.length > 2)
-                              Transform.scale(
-                                scale: 0.65,
-                                child: PodiumAvatar(
-                                  profileImage: _users[2].avatar,
-                                  rank: 3,
-                                  username: _users[2].username,
-                                  points: _users[2].points,
-                                  mastery: _users[2].mastery,
-                                  level: _users[2].level,
-                                ),
-                              ),
-                          ],
+                          ElevatedButton(
+                            onPressed: () => _toggleFilter(false),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: _filterFriends
+                                  ? Colors.grey[300]
+                                  : const Color.fromARGB(255, 1, 167, 164),
+                            ),
+                            child: const Text('All'),
+                          ),
+                          const SizedBox(width: 10),
+                          ElevatedButton(
+                            onPressed: () => _toggleFilter(true),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: _filterFriends
+                                  ? const Color.fromARGB(255, 1, 167, 164)
+                                  : Colors.grey[300],
+                            ),
+                            child: const Text('Friends'),
+                          ),
                         ],
                       ),
-                    ),
-                    Center(
-                      child: Text(
-                        'Congrats, you are in position #$_userPosition!',
-                        style: const TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    const SizedBox(height: 15),
-                    // Lista de usuarios restantes
-                    Expanded(
-                      child: SingleChildScrollView(
-                        child: Column(
+                      // Usuarios en el podio
+                      SizedBox(
+                        height: 250,
+                        width: double.infinity,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
-                            ListView.builder(
-                              shrinkWrap: true,
-                              physics: const NeverScrollableScrollPhysics(),
-                              itemCount:
-                                  _users.length > 3 ? _users.length - 3 : 0,
-                              itemBuilder: (context, index) {
-                                final user = _users[index + 3];
-                                return Card(
-                                  color: greenyColor,
-                                  elevation: 2,
-                                  margin: const EdgeInsets.symmetric(
-                                      vertical: 5, horizontal: 0),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(10),
-                                    child: Row(
-                                      children: [
-                                        Text(
-                                          '${index + 4}',
-                                          style: const TextStyle(
-                                              fontSize: 16,
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                        const SizedBox(width: 10),
-                                        CircleAvatar(
-                                          backgroundImage:
-                                              NetworkImage(user.avatar),
-                                          radius: 25,
-                                        ),
-                                        const SizedBox(width: 10),
-                                        Expanded(
-                                          child: Container(
-                                            child: Column(
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              children: [
-                                                Text(
-                                                  user.username,
-                                                  overflow:
-                                                      TextOverflow.ellipsis,
-                                                  style: const TextStyle(
-                                                    fontSize: 18,
-                                                    fontWeight: FontWeight.bold,
-                                                  ),
-                                                ),
-                                                Row(
-                                                  children: [
-                                                    Icon(
-                                                      Icons.star,
-                                                      color: Colors.grey[300],
-                                                      size: 20,
-                                                    ),
-                                                    Text(
-                                                      user.level.toString(),
-                                                      style: const TextStyle(
-                                                          fontSize: 16),
-                                                    ),
-                                                    Icon(
-                                                      Icons
-                                                          .military_tech_rounded,
-                                                      color: Colors.grey[300],
-                                                      size: 20,
-                                                    ),
-                                                    Text(
-                                                      toRoman(user.mastery + 1),
-                                                      style: const TextStyle(
-                                                          fontSize: 16),
-                                                    ),
-                                                  ],
-                                                ),
-                                              ],
-                                            ),
-                                          ),
-                                        ),
-                                        const SizedBox(width: 10),
-                                        Text(
-                                          '${user.points} pts',
-                                          style: const TextStyle(fontSize: 16),
-                                        ),
-                                      ],
-                                    ),
+                            if (_users != null) ...[
+                              if (_users.length > 1)
+                                Transform.scale(
+                                  scale: 0.65,
+                                  child: PodiumAvatar(
+                                    profileImage: _users[1].avatar,
+                                    rank: 2,
+                                    username: _users[1].username,
+                                    points: _users[1].points,
+                                    mastery: _users[1].mastery,
+                                    level: _users[1].level,
                                   ),
-                                );
-                              },
-                            ),
+                                ),
+                              if (_users.isNotEmpty)
+                                PodiumAvatar(
+                                  profileImage: _users[0].avatar,
+                                  rank: 1,
+                                  username: _users[0].username,
+                                  points: _users[0].points,
+                                  mastery: _users[0].mastery,
+                                  level: _users[0].level,
+                                ),
+                              if (_users.length > 2)
+                                Transform.scale(
+                                  scale: 0.65,
+                                  child: PodiumAvatar(
+                                    profileImage: _users[2].avatar,
+                                    rank: 3,
+                                    username: _users[2].username,
+                                    points: _users[2].points,
+                                    mastery: _users[2].mastery,
+                                    level: _users[2].level,
+                                  ),
+                                ),
+                            ],
                           ],
                         ),
                       ),
-                    ),
-                  ],
+                      Center(
+                        child: Text(
+                          'Congrats, you are in position #$_userPosition!',
+                          style: const TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+                      // Lista de usuarios restantes
+                      Expanded(
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              ListView.builder(
+                                shrinkWrap: true,
+                                physics: const NeverScrollableScrollPhysics(),
+                                itemCount:
+                                    _users.length > 3 ? _users.length - 3 : 0,
+                                itemBuilder: (context, index) {
+                                  final user = _users[index + 3];
+                                  return Card(
+                                    color: greenyColor,
+                                    elevation: 2,
+                                    margin: const EdgeInsets.symmetric(
+                                        vertical: 5, horizontal: 0),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(10),
+                                      child: Row(
+                                        children: [
+                                          Text(
+                                            '${index + 4}',
+                                            style: const TextStyle(
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          const SizedBox(width: 10),
+                                          CircleAvatar(
+                                            backgroundImage:
+                                                NetworkImage(user.avatar),
+                                            radius: 25,
+                                          ),
+                                          const SizedBox(width: 10),
+                                          Expanded(
+                                            child: Container(
+                                              child: Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  Text(
+                                                    user.username,
+                                                    overflow:
+                                                        TextOverflow.ellipsis,
+                                                    style: const TextStyle(
+                                                      fontSize: 18,
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                    ),
+                                                  ),
+                                                  Row(
+                                                    children: [
+                                                      Icon(
+                                                        Icons.star,
+                                                        color: Colors.grey[300],
+                                                        size: 20,
+                                                      ),
+                                                      Text(
+                                                        user.level.toString(),
+                                                        style: const TextStyle(
+                                                            fontSize: 16),
+                                                      ),
+                                                      Icon(
+                                                        Icons
+                                                            .military_tech_rounded,
+                                                        color: Colors.grey[300],
+                                                        size: 20,
+                                                      ),
+                                                      Text(
+                                                        toRoman(
+                                                            user.mastery + 1),
+                                                        style: const TextStyle(
+                                                            fontSize: 16),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                          const SizedBox(width: 10),
+                                          Text(
+                                            '${user.points} pts',
+                                            style:
+                                                const TextStyle(fontSize: 16),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
